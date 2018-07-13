11121c91bbb638e9f837771f35b73405dc6dde09
Remco Meeuwissen <hello@kalm.io>
2018-07-14 00:07:37 +0200

    Moved the component CSS into the JS file using the styled components library

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react'
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg'
-import './App.css'
 
+const StyledApp = styled.div`
+  text-align: center;
+`
+
+const RotateLogo = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Logo = styled.img`
+  animation: ${RotateLogo} infinite 20s linear;
+  height: 80px;
+`
+
+const Header = styled.div`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+
+const HeaderTitle = styled.h1`
+  font-size: 1.5em;
+`
+
+const Intro = styled.div`
+  font-size: large;
+`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <StyledApp>
+        <Header>
+          <Logo src={logo} alt="logo" />
+          <HeaderTitle>Welcome to React</HeaderTitle>
+        </Header>
+
+        <Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </Intro>
+      </StyledApp>
     )
   }
 }
