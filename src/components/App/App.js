289dfa76bc4295fd77f5ce0c85bdf95ca7ac256d
Remco Meeuwissen <hello@kalm.io>
2018-07-16 01:40:37 +0200

    Updated App.js to show the new Maps component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,51 +1,16 @@
 import React, { Component } from 'react'
-import styled, { keyframes } from 'styled-components'
-import logo from './logo.svg'
+import styled from 'styled-components'
+import { Maps } from '../../containers'
 
 const StyledApp = styled.div`
-  text-align: center;
+  margin: 1em;
 `
 
-const RotateLogo = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`
-
-const Logo = styled.img`
-  animation: ${RotateLogo} infinite 20s linear;
-  height: 80px;
-`
-
-const Header = styled.div`
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  color: white;
-`
-
-const HeaderTitle = styled.h1`
-  font-size: 1.5em;
-`
-
-const Intro = styled.div`
-  font-size: large;
-`
 class App extends Component {
   render() {
     return (
       <StyledApp>
-        <Header>
-          <Logo src={logo} alt="logo" />
-          <HeaderTitle>Welcome to React</HeaderTitle>
-        </Header>
-
-        <Intro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </Intro>
+        <Maps />
       </StyledApp>
     )
   }
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from '../../reducers'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  const store = createStore(rootReducer)
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
