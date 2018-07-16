a29ab937f92020a99b3a6267bebcf21af1fa3b41
Remco Meeuwissen <hello@kalm.io>
2018-07-17 00:58:45 +0200

    Increased test coverage for the Map component

--- src/components/Maps/Maps.test.js
+++ src/components/Maps/Maps.test.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Maps from './Maps'
+import MapCard from '../MapCard'
 import mapReducer from '../../reducers/maps'
+import { addMap } from '../../actions/MapActions'
 
-it('renders without crashing', () => {
-  const initialState = mapReducer(undefined, {})
-  const wrapper = shallow(<Maps maps={initialState} />)
+describe('Maps component', () => {
+  it('renders without crashing', () => {
+    const initialState = mapReducer(undefined, {})
+    const wrapper = shallow(<Maps maps={initialState} />)
 
-  expect(wrapper).toBeTruthy()
+    expect(wrapper).toBeTruthy()
+  })
+
+  it('shows cards for the maps passed to it', () => {
+    const addMapAction = addMap('First map')
+    const secondAddMapAction = addMap('Second map')
+
+    let initialState = mapReducer(undefined, addMapAction)
+    initialState = mapReducer(initialState, secondAddMapAction)
+
+    const wrapper = shallow(<Maps maps={initialState} />)
+
+    expect(wrapper.contains(<MapCard name="First map" />)).toEqual(true)
+    expect(wrapper.contains(<MapCard name="Second map" />)).toEqual(true)
+  })
 })
